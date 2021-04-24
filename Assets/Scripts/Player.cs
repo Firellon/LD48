637b809e04cfbe0954e72a6a0faec86c39502846
Firellon <macfirellon@gmail.com>
2021-04-24 10:55:05 +0200

    Basic Human control and Tree collission

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,15 +4,33 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private Rigidbody2D body;
+
+    private float horizontal;
+    private float vertical;
+
+    public float runSpeed = 20.0f;
+
+    private void Start ()
     {
-        
+        body = GetComponent<Rigidbody2D>(); 
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update ()
     {
-        
+        horizontal = Input.GetAxisRaw("Horizontal");
+        vertical = Input.GetAxisRaw("Vertical"); 
     }
+
+    private void FixedUpdate()
+    {
+        body.velocity = new Vector2(horizontal * runSpeed, vertical * runSpeed);
+    }
+
+    private void OnCollisionEnter2D(Collision2D hit){
+        if(hit.gameObject.CompareTag("Wall"))
+        {
+            body.velocity = Vector2.zero;
+        }
+    } 
 }
