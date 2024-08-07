da775ba72a5dd3537d091f033a552fa8fa68bc40
Firellon <macfirellon@gmail.com>
2024-08-07 22:54:37 +0200

    fix: bullet must vanish when colliding with MapObjects

--- Assets/Scripts/Human/HumanController.cs
+++ Assets/Scripts/Human/HumanController.cs
@@ -431,7 +431,7 @@ namespace Human
 
             var bulletObject = prefabPool.Spawn(bulletPrefab, transform);
             var xDirection = spriteRenderer.flipX ? -1 : 1;
-            var bullet = bulletObject.GetComponent<Bullet>();
+            var bullet = bulletObject.GetComponent<BulletController>();
             bullet.SetDirection(new Vector2(xDirection, 0));
 
             bulletObject.transform.localPosition = bulletPosition * new Vector2(xDirection, 1);
